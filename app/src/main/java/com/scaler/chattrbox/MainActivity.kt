26b5f6a94ab3d3e7392fac42df2c77ebf6200822
Arnav Gupta <arnav@codingblocks.com>
2021-08-22 18:26:58 +0530

    002: added Firebase auth libraries and setup
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/scaler/chattrbox/MainActivity.kt
+++ app/src/main/java/com/scaler/chattrbox/MainActivity.kt
@@ -2,11 +2,56 @@ package com.scaler.chattrbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.scaler.chattrbox.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private val auth = FirebaseAuth.getInstance()
+    private val loginCallbacks = object: PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+            Toast.makeText(this@MainActivity, "VERIFICATION COMPLETED", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onVerificationFailed(p0: FirebaseException) {
+            Toast.makeText(this@MainActivity, "VERIFICATION FAILED", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+            Toast.makeText(this@MainActivity, "VERIFICATION CODE SENT", Toast.LENGTH_SHORT).show()
+            _binding.otpEditText.isVisible = true
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        _binding.phoneLoginButton.setOnClickListener {
+            initiatePhoneLogin(_binding.phoneNumberEditText.text.toString())
+        }
+
+    }
+
+    fun initiatePhoneLogin (phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)       // Phone number to verify
+                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                .setActivity(this)                 // Activity (for callback binding)
+                .setCallbacks(loginCallbacks)          // OnVerificationStateChangedCallbacks
+                .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
