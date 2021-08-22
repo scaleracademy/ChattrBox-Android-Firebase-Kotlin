2a5170b575c10d47d65dfb555342a249a7b03591
Arnav Gupta <arnav@codingblocks.com>
2021-08-22 18:48:34 +0530

    003: Phone login is working correctly
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/scaler/chattrbox/MainActivity.kt
+++ app/src/main/java/com/scaler/chattrbox/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.scaler.chattrbox
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.*
 import com.scaler.chattrbox.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "AUTH"
+    }
 
     private lateinit var _binding: ActivityMainBinding
     private val auth = FirebaseAuth.getInstance()
-    private val loginCallbacks = object: PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+    private val loginCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
         override fun onVerificationCompleted(p0: PhoneAuthCredential) {
             Toast.makeText(this@MainActivity, "VERIFICATION COMPLETED", Toast.LENGTH_SHORT).show()
@@ -29,7 +28,16 @@ class MainActivity : AppCompatActivity() {
 
         override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
             Toast.makeText(this@MainActivity, "VERIFICATION CODE SENT", Toast.LENGTH_SHORT).show()
-            _binding.otpEditText.isVisible = true
+            updateUIState(UIState.ENTER_OTP)
+
+            _binding.verifyOtpButton.setOnClickListener {
+                val credential = PhoneAuthProvider.getCredential(
+                        verificationId,
+                        _binding.otpEditText.text.toString()
+                )
+                signInWithPhoneAuthCredential(credential)
+
+            }
         }
 
     }
@@ -42,10 +50,14 @@ class MainActivity : AppCompatActivity() {
         _binding.phoneLoginButton.setOnClickListener {
             initiatePhoneLogin(_binding.phoneNumberEditText.text.toString())
         }
+        _binding.resetButton.setOnClickListener {
+            updateUIState(UIState.ENTER_PHONE)
+        }
+
 
     }
 
-    fun initiatePhoneLogin (phoneNumber: String) {
+    fun initiatePhoneLogin(phoneNumber: String) {
         val options = PhoneAuthOptions.newBuilder(auth)
                 .setPhoneNumber(phoneNumber)       // Phone number to verify
                 .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
@@ -54,4 +66,58 @@ class MainActivity : AppCompatActivity() {
                 .build()
         PhoneAuthProvider.verifyPhoneNumber(options)
     }
+
+    enum class UIState {
+        ENTER_PHONE,
+        ENTER_OTP
+    }
+
+    fun updateUIState(uiState: UIState) = when (uiState) {
+        UIState.ENTER_PHONE -> {
+            _binding.phoneNumberEditText.apply {
+                isEnabled = true
+                setText("")
+            }
+            _binding.otpEditText.apply {
+                isEnabled = false
+                isVisible = false
+            }
+            _binding.phoneLoginButton.isEnabled = true
+            _binding.resetButton.isEnabled = false
+            _binding.verifyOtpButton.isEnabled = false
+        }
+        UIState.ENTER_OTP -> {
+            _binding.phoneNumberEditText.isEnabled = false
+            _binding.otpEditText.apply {
+                isVisible = true
+                isEnabled = true
+                setText("")
+            }
+            _binding.phoneLoginButton.isEnabled = false
+            _binding.resetButton.isEnabled = true
+            _binding.verifyOtpButton.isEnabled = true
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+                        val user = task.result?.user
+                        // TODO: move to the chat screen
+
+                    } else {
+                        Toast.makeText(this, "LOGIN FAILED", Toast.LENGTH_SHORT).show()
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            Toast.makeText(this, "WRONG OTP", Toast.LENGTH_SHORT).show()
+                        }
+                        updateUIState(UIState.ENTER_PHONE)
+
+                    }
+                }
+    }
+
 }
